91624d8f7319f19329c6c0665163346eb459fcc0
LMDEHJULIO <juliodevelops@gmail.com>
2018-07-20 04:26:48 -0400

    Initiating routing

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://julio:julioio7@ds141661.mlab.com:41661/job-jawn"
+};
--- server.js
+++ server.js
@@ -1,7 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+
+const users = require("./routes/api/profile");
+
+const users = require("./routes/api/posts");
 
 const app = express();
 
+// DB Config
+
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => {
-  res.send("Testing");
+  res.send("Hi how ya dern!");
 });
+
+// Use Routes
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
